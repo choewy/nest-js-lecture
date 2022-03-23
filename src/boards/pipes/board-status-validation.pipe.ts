6fad58204985098db0f194554b1f4b4c090e4d5d
choewy <choewy32@gmail.com>
2022-03-23 17:01:40 +0900

    BadRequestException : updateBoardStatus

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,23 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../board.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly statusOptions = [BoardStatus.PUBLIC, BoardStatus.PRIVATE];
+
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(
+        `${value}는 게시물의 공개여부로 설정할 수 없습니다.`,
+      );
+    }
+
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    const index = this.statusOptions.indexOf(status);
+    return index !== -1;
+  }
+}
