207c942e4b0c3188d6d8f2548128346e6d95ac9e
choewy <choewy32@gmail.com>
2022-03-24 14:56:02 +0900

    PostgresSQL 사용 전 코드 재정비

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -9,7 +9,7 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
@@ -19,31 +19,31 @@ export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
   @Get()
-  getAllBoards(): Board[] {
-    return this.boardsService.getAllBoards();
+  getAllBoards() {
+    // return this.boardsService.getAllBoards();
   }
 
   @Get('/:id')
-  getBoardById(@Param('id') id: string): Board {
-    return this.boardsService.getBoardById(id);
+  getBoardById(@Param('id') id: string) {
+    // return this.boardsService.getBoardById(id);
   }
 
   @Delete('/:id')
   deleteBoardById(@Param('id') id: string): void {
-    this.boardsService.deleteBoardById(id);
+    // this.boardsService.deleteBoardById(id);
   }
 
   @Post()
   @UsePipes(ValidationPipe)
-  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
-    return this.boardsService.createBoard(createBoardDto);
+  createBoard(@Body() createBoardDto: CreateBoardDto) {
+    // return this.boardsService.createBoard(createBoardDto);
   }
 
   @Patch('/:id/status')
   updateBoardStatus(
     @Param('id') id: string,
     @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-  ): Board {
-    return this.boardsService.updateBoardStatusById(id, status);
+  ) {
+    // return this.boardsService.updateBoardStatusById(id, status);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,53 +1,46 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
-  private boards: Board[] = [
-    {
-      id: '0aa62910-aa50-11ec-ac11-4bbf39811cac',
-      title: '테스트 입니다.',
-      description: '테스트의 내용입니다.',
-      status: BoardStatus.PUBLIC,
-    },
-  ];
-
-  getAllBoards(): Board[] {
-    return this.boards;
-  }
-
-  getBoardById(id: string): Board {
-    const found = this.boards.find((board) => board.id === id);
-
-    if (!found) {
-      throw new NotFoundException('존재하지 않는 게시물입니다.');
-    }
-    return found;
-  }
-
-  deleteBoardById(id: string): void {
-    const found = this.getBoardById(id);
-    this.boards = this.boards.filter((board) => board.id !== found.id);
-  }
-
-  createBoard(createBoardDto: CreateBoardDto) {
-    const { title, description } = createBoardDto;
-    const board: Board = {
-      id: uuid(),
-      title,
-      description,
-      status: BoardStatus.PUBLIC,
-    };
-
-    this.boards.push(board);
-    return board;
-  }
-
-  updateBoardStatusById(id: string, status: BoardStatus): Board {
-    const board = this.getBoardById(id);
-    board.status = status;
-    return board;
-  }
+  //   private boards: Board[] = [
+  //     {
+  //       id: '0aa62910-aa50-11ec-ac11-4bbf39811cac',
+  //       title: '테스트 입니다.',
+  //       description: '테스트의 내용입니다.',
+  //       status: BoardStatus.PUBLIC,
+  //     },
+  //   ];
+  //   getAllBoards(): Board[] {
+  //     return this.boards;
+  //   }
+  //   getBoardById(id: string): Board {
+  //     const found = this.boards.find((board) => board.id === id);
+  //     if (!found) {
+  //       throw new NotFoundException('존재하지 않는 게시물입니다.');
+  //     }
+  //     return found;
+  //   }
+  //   deleteBoardById(id: string): void {
+  //     const found = this.getBoardById(id);
+  //     this.boards = this.boards.filter((board) => board.id !== found.id);
+  //   }
+  //   createBoard(createBoardDto: CreateBoardDto) {
+  //     const { title, description } = createBoardDto;
+  //     const board: Board = {
+  //       id: uuid(),
+  //       title,
+  //       description,
+  //       status: BoardStatus.PUBLIC,
+  //     };
+  //     this.boards.push(board);
+  //     return board;
+  //   }
+  //   updateBoardStatusById(id: string, status: BoardStatus): Board {
+  //     const board = this.getBoardById(id);
+  //     board.status = status;
+  //     return board;
+  //   }
 }
