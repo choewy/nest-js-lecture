6123cca23d7eab68b69f7f039ce95e99f79479e4
최원영 <70950533+choewy@users.noreply.github.com>
2022-03-24 19:32:43 +0900

    #4 Custom Repository  -> Repository 변경
    #4 해결한 이후 Custom Repository로 변경 필요

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,82 +1,14 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { BoardStatus } from './board-status.enum';
-import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { BoardRepository } from './board.repository';
 import { Board } from './board.entity';
-import { FindOneOptions } from 'typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class BoardsService {
-  // Inject Repository to Service
   constructor(
-    @InjectRepository(BoardRepository)
-    private boardRepository: BoardRepository,
+    @InjectRepository(Board)
+    private boardRepository: Repository<Board>,
   ) {}
-
-  async getAllBoards(): Promise<Board[]> {
-    return await this.boardRepository.find();
-  }
-
-  async getBoardById(id: number): Promise<Board> {
-    const found = await this.boardRepository.findOne({ where: { id } });
-    if (!found) {
-      throw new NotFoundException(
-        `{id: ${id}}에 해당하는 게시물을 찾을 수 없습니다.`,
-      );
-    }
-    return found;
-  }
-
-  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
-    const { title, description } = createBoardDto;
-    const board = this.boardRepository.create({
-      title,
-      description,
-      status: BoardStatus.PUBLIC,
-    });
-
-    await this.boardRepository.save(board);
-    return board;
-  }
-
-  //   private boards: Board[] = [
-  //     {
-  //       id: '0aa62910-aa50-11ec-ac11-4bbf39811cac',
-  //       title: '테스트 입니다.',
-  //       description: '테스트의 내용입니다.',
-  //       status: BoardStatus.PUBLIC,
-  //     },
-  //   ];
-  //   getAllBoards(): Board[] {
-  //     return this.boards;
-  //   }
-  //   getBoardById(id: string): Board {
-  //     const found = this.boards.find((board) => board.id === id);
-  //     if (!found) {
-  //       throw new NotFoundException('존재하지 않는 게시물입니다.');
-  //     }
-  //     return found;
-  //   }
-  //   deleteBoardById(id: string): void {
-  //     const found = this.getBoardById(id);
-  //     this.boards = this.boards.filter((board) => board.id !== found.id);
-  //   }
-  //   createBoard(createBoardDto: CreateBoardDto) {
-  //     const { title, description } = createBoardDto;
-  //     const board: Board = {
-  //       id: uuid(),
-  //       title,
-  //       description,
-  //       status: BoardStatus.PUBLIC,
-  //     };
-  //     this.boards.push(board);
-  //     return board;
-  //   }
-  //   updateBoardStatusById(id: string, status: BoardStatus): Board {
-  //     const board = this.getBoardById(id);
-  //     board.status = status;
-  //     return board;
-  //   }
 }
