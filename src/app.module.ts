c82714cc49aa1f2089c27609df12d7cf67965c86
choewy <choewy32@gmail.com>
2022-04-25 16:17:41 +0900

    [추가] TypeORM 설치 및 모듈 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { UsersModule } from './user/users.module';
 import { EmailModule } from './email/email.module';
 import { ConfigModule } from '@nestjs/config';
 import emailConfig from './config/email.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 const envDirPath = `${__dirname}/config/env`;
 const envFiles = () =>
@@ -19,6 +20,16 @@ const envFiles = () =>
       envFilePath: envFiles(),
       load: [emailConfig],
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: Boolean(process.env.DATABASE_SYNC),
     }),
     UsersModule,
     EmailModule,
