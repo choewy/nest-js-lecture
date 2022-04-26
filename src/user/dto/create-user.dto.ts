aadc840e2888b3c7a2dcce84bf55006ee06a8e0d
choewy <choewy32@gmail.com>
2022-04-26 14:01:10 +0900

    [수정] CreateUserDto에 Custom Decorator 적용

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,30 +1,20 @@
-import { BadRequestException } from '@nestjs/common';
-import { Transform } from 'class-transformer';
-import {
-  IsEmail,
-  IsString,
-  Matches,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
-import { IsNotIncludes } from '../pipe/is-not-includes.pipe';
+import { UsersDtoDecorators } from '../users.configs';
+
+const Decorators = new UsersDtoDecorators();
 
 export class CreateUserDto {
-  @Transform((params) => params.value.trim())
-  @IsNotIncludes('password', {
-    message: '비밀번호에는 이름과 같은 문자열을 포함할 수 없습니다.',
-  })
-  @IsString()
-  @MinLength(2)
-  @MaxLength(30)
-  readonly name: string;
-
-  @IsString()
-  @IsEmail()
-  @MaxLength(60)
+  @Decorators.IsEmailString()
+  @Decorators.IsEmailValid()
+  @Decorators.EmailMaxLength()
   readonly email: string;
 
-  @IsString()
-  @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/)
+  @Decorators.NameTransForm()
+  @Decorators.IsNameNotIncludes()
+  @Decorators.NameMinLength()
+  @Decorators.NameMaxLength()
+  readonly name: string;
+
+  @Decorators.PasswordIsString()
+  @Decorators.PasswordMatches()
   readonly password: string;
 }
