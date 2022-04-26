9112e56558ca507cc16cea73338d8fde802aa607
choewy <choewy32@gmail.com>
2022-04-26 14:01:54 +0900

    [추가] 예외 처리 클래스 의존성 주입

--- src/user/users.module.ts
+++ src/user/users.module.ts
@@ -3,10 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
+import { UsersException } from './users.configs';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UsersException],
 })
 export class UsersModule {}
--- src/user/users.service.ts
+++ src/user/users.service.ts
@@ -7,6 +7,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
 import { Connection, Repository } from 'typeorm';
 import { ulid } from 'ulid';
+import { UsersException } from './users.configs';
 
 @Injectable()
 export class UsersService {
@@ -14,61 +15,35 @@ export class UsersService {
     @InjectRepository(UserEntity)
     private usersRepository: Repository<UserEntity>,
     private connection: Connection,
+    private exceptions: UsersException,
   ) {}
-
-  async createUser(createUserDto: CreateUserDto) {
-    const { email } = createUserDto;
-    const isExist = await this.checkUserExists(email);
-
-    if (isExist) {
-      throw new UnprocessableEntityException(
-        '이미 존재하는 이메일 계정입니다.',
-      );
-    }
-
-    const signupVerifyToken = uuid.v1();
-    await this.saveUser(createUserDto, signupVerifyToken);
-    // await this.verifyEmail(email, signupVerifyToken);
-  }
 
   async checkUserExists(email: string): Promise<boolean> {
     const user = await this.usersRepository.findOne({ where: { email } });
     return user !== null;
   }
 
-  async saveUser(createUserDto: CreateUserDto, signupVerifyToken: string) {
-    /* 트랜잭션 처리를 위한 queryRunner 객체 생성 */
-    const queryRunner = this.connection.createQueryRunner();
+  async createUser(createUserDto: CreateUserDto) {
+    const { email } = createUserDto;
+    const isExist = await this.checkUserExists(email);
+    if (isExist) this.exceptions.AlreadyExistUser();
 
-    /* DB 연결 후 트랜젝션 시작 */
+    const queryRunner = this.connection.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
 
     try {
-      const { name, email, password } = createUserDto;
       const user = new UserEntity();
       user.id = ulid();
-      user.name = name;
-      user.email = email;
-      user.password = password;
-      user.signupVerifyToken = signupVerifyToken;
+      user.password = 'HASHED PASSWORD';
       await queryRunner.manager.save(user);
       await queryRunner.commitTransaction();
     } catch (e) {
-      /* 오류 발생 시 롤백 처리 */
       await queryRunner.rollbackTransaction();
     } finally {
-      /* 반드시 queryRunner 객체 해제 */
       await queryRunner.release();
     }
   }
-
-  // private async verifyEmail(
-  //   email: string,
-  //   signupVerifyToken: string,
-  // ): Promise<string> {
-  //   return this.emailService.sendMemberJoinEmail(email, signupVerifyToken);
-  // }
 
   async login(userLoginDto: UserLoginDto): Promise<string> {
     console.log(userLoginDto);
