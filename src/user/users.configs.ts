fc659058cd742e7d47848e679c6bcab56b88fae1
choewy <choewy32@gmail.com>
2022-04-26 14:00:34 +0900

    [추가] UsersModule 전반적으로 사용할 Decorator와 Errors를 별도의 클래스로 분할

---
+++ src/user/users.configs.ts
@@ -0,0 +1,49 @@
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsNotIncludes } from './pipe/is-not-includes.pipe';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+
+export class UsersDtoDecorators {
+  IsEmailString = () => IsString({ message: '이메일 형식에 맞지 않습니다.' });
+  IsEmailValid = () => IsEmail({}, { message: '이메일 형식에 맞지 않습니다.' });
+  EmailMaxLength = () =>
+    MaxLength(60, { message: '이메일은 60자 이내로 입력하세요.' });
+
+  NameTransForm = () => Transform((parmas) => parmas.value.trim());
+  IsNameNotIncludes = () =>
+    IsNotIncludes('password', {
+      message: '이름과 같은 문자열을 포함시킬 수 없습니다.',
+    });
+  IsNameString = () =>
+    IsString({
+      message: '이름 형식에 맞지 않습니다.',
+    });
+  NameMinLength = () =>
+    MinLength(2, {
+      message: '이름은 2-30자 이내로 입력하세요.',
+    });
+  NameMaxLength = () =>
+    MinLength(30, {
+      message: '이름은 2-30자 이내로 입력하세요.',
+    });
+
+  PasswordIsString = () =>
+    IsString({ message: '비밀번호 형식에 맞지 않습니다.' });
+  PasswordMatches = () =>
+    Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/, {
+      message: '비밀번호 형식에 맞지 않습니다.',
+    });
+}
+
+@Injectable()
+export class UsersException {
+  AlreadyExistUser = () => {
+    throw new UnprocessableEntityException('이미 존재하는 이메일 계정입니다.');
+  };
+}
